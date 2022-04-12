1afbccba9d1b1bb2ed70f7d3014c90859ea71644
Guillaume Tomasi <guillaume.tomasi.76@gmail.com>
2022-04-12 16:07:17 +0200

    ajout panier

--- front/js/product.js
+++ front/js/product.js
@@ -1,7 +1,17 @@
 let product = [];
+
 let params = new URLSearchParams(document.location.search);
 let id = params.get("id");
+let quantityInput = document.querySelector('#quantity');
+let colorSelected = document.querySelector("#colors");
 
+let cartInfo = {
+    id: "",
+    quantityItem: "",
+    colorItem: "",
+};
+
+// Récupération du produit + stockage dans la variable
 
 async function fetchApi() {
     await fetch(`http://localhost:3000/api/products/${id}`)
@@ -12,6 +22,8 @@ async function fetchApi() {
             product = data;
         })
 }
+
+// Implémentation du produit sur la page
 
 async function productDisplay() {
     await fetchApi();
@@ -27,12 +39,41 @@ async function productDisplay() {
         }
     }
     colorsList();
-
-
-
-
-
-
 }
 
 productDisplay();
+
+
+function ProductAddInfo() {
+
+    document.getElementById("addToCart").addEventListener("click", function () {
+
+        cartInfo.id = product._id;
+        cartInfo.quantityItem = quantityInput.value;
+        cartInfo.colorItem = colorSelected.value;
+
+        let storageAdd = JSON.parse(localStorage.getItem("cart"));
+
+        if (storageAdd) {
+            let searchItem = storageAdd.find((element) => element.id == cartInfo.id && element.colorItem == cartInfo.colorItem);
+
+            if (searchItem) {
+                searchItem.quantityItem += cartInfo.quantityItem;
+                localStorage.setItem("cart", JSON.stringify(storageAdd));
+                console.log(storageAdd);
+            }
+        } else {
+            let storageAdd = [];
+            storageAdd.push(cartInfo);
+            localStorage.setItem("cart", JSON.stringify(storageAdd)); console.log(storageAdd);
+        }
+    }
+
+    )
+
+}
+ProductAddInfo();
+
+
+
+
